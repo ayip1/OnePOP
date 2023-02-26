aa0c6e19290f13f0b63d9161ba14c25d9607a85f
Dustin Lee <djl291@scarletmail.rutgers.edu>
2023-02-26 18:38:10 -0500

    changed registration UI, added backend

--- app/src/main/java/com/example/myloginapp/DatabaseHandler.java
+++ app/src/main/java/com/example/myloginapp/DatabaseHandler.java
@@ -17,7 +17,7 @@ public class DatabaseHandler {
             cs.executeQuery();
 
             int user_id = cs.getInt(3);
-
+            con.close();
             if (user_id>0) return true;
 
         } catch (Exception e) {
@@ -25,6 +25,71 @@ public class DatabaseHandler {
         }
 
         return false;
+    }
+
+    public static void registerUser(String firstname, String lastname, String username, String password, String email) {
+        ApplicationDB db = new ApplicationDB();
+        Connection con = db.getConnection();
+        PreparedStatement ps;
+        try {
+            String statement = "INSERT INTO user (first_name, last_name, username, password, email)"
+                    + " VALUES (?, ?, ?, ?, ?)";
+            ps = con.prepareStatement(statement);
+            ps.setString(1, firstname);
+            ps.setString(2, lastname);
+            ps.setString(3, username);
+            ps.setString(4, password);
+            ps.setString(5, email);
+            ps.execute();
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static boolean uniqueUsername(String username) {
+        ApplicationDB db = new ApplicationDB();
+        Connection con = db.getConnection();
+        PreparedStatement ps;
+        ResultSet result;
+
+        try {
+            String statement = "SELECT * FROM user WHERE username=?";
+            ps = con.prepareCall(statement);
+            ps.setString(1, username);
+            result = ps.executeQuery();
+            con.close();
+            if (result.next()) return false;
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    public static boolean uniqueEmail(String email) {
+        ApplicationDB db = new ApplicationDB();
+        Connection con = db.getConnection();
+        PreparedStatement ps;
+        ResultSet result;
+
+        try {
+            String statement = "SELECT * FROM user WHERE email=?";
+            ps = con.prepareCall(statement);
+            ps.setString(1, email);
+            result = ps.executeQuery();
+            con.close();
+            if (result.next()) return false;
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
     }
 
 }
